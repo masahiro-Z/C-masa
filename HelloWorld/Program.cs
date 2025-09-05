bb3dcd171cadc4f1b7446d19a9102622c6a9cd5e
諒弘 銭谷 <masahirozenitani@gmail.com>
2025-09-06 01:12:09 +0900

    課題

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,14 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   // ①
             Console.WriteLine("Hello World!");
+
+
+            // ②
+            Console.WriteLine("こんにちは、");
+            Console.WriteLine("はじめまして");
+
+
             Console.ReadKey(true);
         }
     }
 }
+
+
+
+          
+
+
+        
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
